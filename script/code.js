b9c4e94c99d4be985924d751dbe8a825b1b32341
Nusaibah01 <charifnusaibah01@gmail.com>
2024-06-04 14:35:49 +0200

    ct

---
+++ script/code.js
@@ -0,0 +1,5 @@
+// Counting truthy
+let myArray = [true, false, true, false, false, true]
+let countingTruthy = myArray.filter(value => value == true).length
+console.log(myArray)
+console.log(countingTruthy)
